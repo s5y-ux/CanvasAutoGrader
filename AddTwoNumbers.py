346f747c29bf41d37564f65df09d4418ff7721f9
Joseph Shumaker <59636597+s5y-ux@users.noreply.github.com>
2024-02-27 18:23:53 -0800

    Update AddTwoNumbers.py

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,8 +1,8 @@
 #Please put your student ID here: (Not your 900 number)
-student_id = 111283691
+student_id = 
 
 #Do not touch "def test():"
 def test():
 
 	#Code Goes Here: (Make sure it's indented)
-	return(int(input()) + int(input()))
+	
